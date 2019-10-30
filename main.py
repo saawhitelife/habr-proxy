d22b88eeb145136b1218966460a1ea38e1c4028f
Alexander Kremenchuk <saawhitelife@gmail.com>
2019-10-30 22:04:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,26 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import re
 import os
 from requests import get as rget
-from bs4 import BeautifulSoup as bs
 
 
 def create_page_to_display(path):
-    regex = re.compile(r'(\b\w{6}\b)', re.U)
+    regex = re.compile(r'(\b[а-яА-Я]{6}\b)', re.U)
     response = rget('https://habr.com' + str(path))
     open("page.html", 'w').write(response.text)
     with open("page.html", "r") as html_reader:
         content = html_reader.read()
     html_reader.close()
-    new_content = content.replace("https://habr.com",
+    content = content.replace("https://habr.com",
                                   "http://127.0.0.1:8000")
-    soup = bs(new_content, 'html.parser')
-    my_div = soup.find("div", class_="post__wrapper")
-    if my_div:
-        for child in my_div.descendants:
-            if child.name == "script":
-                continue
-            if child.string:
-                child.string = re.sub(regex, r'\1™', child.string)
-    open("page.html", 'w').write(str(soup))
+    content = re.sub(regex, r'\1™', content)
+    open("page.html", 'w').write(content)
+
+
+def handle_file(path):
+    response = rget('https://habr.com' + path)
+    if not os.path.exists("/".join(path.lstrip("/").split("/")[:-1])):
+        os.makedirs("/".join(path.lstrip("/").split("/")[:-1]))
+    open(path.lstrip("/"), 'wb').write(response.content)
 
 
 class Handler(SimpleHTTPRequestHandler):
@@ -30,10 +29,7 @@ class Handler(SimpleHTTPRequestHandler):
         print("SELF PATH %s" % self.path)
         if len(self.path.split(".")) > 1:
             if self.path.endswith("svg"):
-                response = rget('https://habr.com' + self.path)
-                if not os.path.exists("/".join(self.path.lstrip("/").split("/")[:-1])):
-                    os.makedirs("/".join(self.path.lstrip("/").split("/")[:-1]))
-                open(self.path.lstrip("/"), 'wb').write(response.content)
+                handle_file(self.path)
                 file = open(self.path.lstrip("/"), 'rb')
                 self.send_response(200)
                 self.send_header('Content-type', 'image/svg+xml')
@@ -41,10 +37,7 @@ class Handler(SimpleHTTPRequestHandler):
                 self.wfile.write(file.read())
                 file.close()
             elif self.path.endswith("woff2"):
-                response = rget('https://habr.com' + self.path)
-                if not os.path.exists("/".join(self.path.lstrip("/").split("/")[:-1])):
-                    os.makedirs("/".join(self.path.lstrip("/").split("/")[:-1]))
-                open(self.path.lstrip("/"), 'wb').write(response.content)
+                handle_file(self.path)
                 file = open(self.path.lstrip("/"), 'rb')
                 self.send_response(200)
                 self.send_header('Content-type', 'application/font-woff')
@@ -52,10 +45,7 @@ class Handler(SimpleHTTPRequestHandler):
                 self.wfile.write(file.read())
                 file.close()
             elif self.path.endswith("png"):
-                response = rget('https://habr.com' + self.path)
-                if not os.path.exists("/".join(self.path.lstrip("/").split("/")[:-1])):
-                    os.makedirs("/".join(self.path.lstrip("/").split("/")[:-1]))
-                open(self.path.lstrip("/"), 'wb').write(response.content)
+                handle_file(self.path)
                 file = open(self.path.lstrip("/"), 'rb')
                 self.send_response(200)
                 self.send_header('Content-type', 'image/png')
